ed5f10e18d6e095cacb3d5df6b333f921176198e
itsjustkerry <moywaywakerry@gmail.com>
2022-10-15 23:19:43 -0700

    first code using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main (void)
-/**
+{
+ /**
  * puts - prints string with new line
  */
 void puts (void)
-{
-	puts("Programming is like building a multilingual puzzle);
+      {
+	puts(""Programming is like building a multilingual puzzle);
         
         return(0);
-}	
+      }
+}
