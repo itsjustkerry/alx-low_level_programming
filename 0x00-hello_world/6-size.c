efe032b7d2c588ea76b28da867c04e715bf45ee0
itsjustkerry <moywaywakerry@gmail.com>
2022-10-16 12:14:49 -0700

    Prints various size types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Cprogram that prints the size of various types on the computer.
+ *main - Cprogram that prints the size of various types on the computer.
  *
- * Return: Always 0.
+ *Return: Always 0.
  */
 int main(void)
 {	
+	char charType;
 	int intType;
-	float floatType;
 	long int longintType;
 	long long int longlongintType;
-	char charType;
+	float floatType;
 
 	/** sizeof evaluates the size of a variable*/
+	printf("Size of char: %zu bytes\n", sizeof(charType));
 	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
 	printf("Size of long int: %zu bytes\n", sizeof(longintType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
-	printf("Size of long long int: %zu bytes\n", sizeof(longlongintType));
+	printf("Size of long long int: %zu byte\n", sizeof(longlongintType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
 
 	return (0);
 }
