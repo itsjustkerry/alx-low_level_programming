aba26ece272aabc881f54ec5505bdb8762faa3f0
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 08:36:07 -0700

    prints digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = a + 1; b <= 9; b++)	
-		{	
+		for (b = a + 1; b <= 9; b++)
+		{
 		putchar((a % 10) + '0');
 		putchar((b % 10) + '0');
 		if (a == 8 && b == 9)
