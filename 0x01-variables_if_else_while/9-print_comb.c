d1ea6ae0a2948a631be631671627abb04fe8f987
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 08:20:07 -0700

    prints combination of numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main-printsallpossiblecombinations
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
