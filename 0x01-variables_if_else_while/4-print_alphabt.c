365cc37ea5d3292f56fdfe2fc33fdd2064fbb72a
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 06:58:38 -0700

    removes q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main-removesqande
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar("ch && !q || !e");
+			putchar('\n');
+	return (0);
+}
