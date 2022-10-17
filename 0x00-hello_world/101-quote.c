028232a523cd94cee9e547ce6be96d3e6e142b6d
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 01:28:09 -0700

    prints standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\"-DoraKorpar, 2015-10-19\n", 59);
+	fwrite(STDOUT_FILENO, "and that piece of art is useful\"-DoraKorpar, 2015-10-19\n", 59);
 
 	return (1);
 }
