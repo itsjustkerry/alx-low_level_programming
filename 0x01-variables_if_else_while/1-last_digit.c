106646a2bcb127e99661ca571a3291c893d38622
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 05:51:54 -0700

    assigns random variables and outputs them

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main-assignsrandomnumbertonandprintswhethergreater,lesserorequaltozero
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 5)
+	{
+		printf("Last digit of n is %d and is greater than 5\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of n is %d and is zero\n", n);
+	}
+	else if (n < 6 && !0)
+	{
+		printf("Last digit of n is %d and is less than 6 and not 0\n", n);
+	}
+	return (0);
+}
