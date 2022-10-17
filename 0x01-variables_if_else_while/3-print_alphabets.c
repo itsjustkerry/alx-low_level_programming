76fa892acc2028234f3ee6a2b0f6368c089f5e31
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 06:42:08 -0700

    prints bth upper and lowercase letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main-printsalphabetinupperthenlowercase
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	int ch
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
