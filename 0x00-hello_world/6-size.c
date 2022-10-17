c5f449fb5f09c9e6640f88efc5dc13e68840bb13
itsjustkerry <moywaywakerry@gmail.com>
2022-10-17 00:35:18 -0700

    prints the size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	float floatType;
 
 	/** sizeof evaluates the size of a variable*/
-	printf("Size of char: %lubyte(s)\n", (unsigned long)sizeof(charType));
-	printf("Size of int: %lubyte(s)\n", (unsigned long)sizeof(intType));
-	printf("Size of long int: %lubyte(s)\n", (unsigned long)sizeof(longintType));
-	printf("Size of long long int: %lubyte\n", (unsigned long)sizeof(longlongintType));
-	printf("Size of float: %lubyte(s)\n", (unsigned long)sizeof(floatType));
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(charType));
+	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(longintType));
+	printf("Size of long long int: %lu byte\n", (unsigned long)sizeof(longlongintType));
+	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 
 	return (0);
 }
