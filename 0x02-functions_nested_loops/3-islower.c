8a594c269f3cc6c300474ebf2394294dddeaf2af
itsjustkerry <moywaywakerry@gmail.com>
2022-10-18 10:40:32 -0700

    checks for lowercase letters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*islower-functionchecksifcharacterislowercase.
+*@c-charactertobechecked.
+*
+*Return: 1ifcislowercase,otherwisereturn0.
+*/
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z');
+		return (1);
+	else
+		return (0);
+}
