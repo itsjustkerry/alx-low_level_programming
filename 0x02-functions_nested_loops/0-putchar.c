16bb33f83364eb491a4c40233c1979e9ed7137d5
itsjustkerry <moywaywakerry@gmail.com>
2022-10-18 08:18:03 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main-printstheline_putchar.
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory is on nested loops.
