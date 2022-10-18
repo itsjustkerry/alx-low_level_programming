b9d4ce395b6dfd7464e48147f3087b939000c95c
itsjustkerry <moywaywakerry@gmail.com>
2022-10-18 12:43:21 -0700

    prints

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*add-afunctionthataddstwointegersandreturnstheresult
+*@one: firstnumberinput
+*@two: secondnumberinput
+*Return: one+two
+*/
+int add(int one, int two)
+{
+	return (one + two);
+}
