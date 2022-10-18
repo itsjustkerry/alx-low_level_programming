46b6f420eb5a5bbd03c4d1b8c8c77c2e615d887b
itsjustkerry <moywaywakerry@gmail.com>
2022-10-18 08:57:00 -0700

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,15 @@
 */
 int main(void)
 {
-	char *k;
-
-	for (k = "_putchar")
-		putchar(k);
-	putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
