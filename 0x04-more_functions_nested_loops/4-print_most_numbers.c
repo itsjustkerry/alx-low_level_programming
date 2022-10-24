d542baab0c6078226aa52a7aaf9c66bb20727faa
itsjustkerry <moywaywakerry@gmail.com>
2022-10-24 07:49:21 -0700

    prints numbers without two and four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,8 +2,8 @@
 
 /**
  *print_most_numbers- prints zero to nine without two and four
- *
- *Return: 0 to 9 without 2 and 4
+ *without 2 and 4
+ *Return: Always 0
  */
 void print_most_numbers(void)
 {
