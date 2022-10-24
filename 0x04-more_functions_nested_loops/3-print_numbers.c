38479f9df27e7c4706f4aa4c7000e3ad6b2500b5
itsjustkerry <moywaywakerry@gmail.com>
2022-10-24 07:08:13 -0700

    prints list of numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,12 @@
 
 /**
  *print_numbers- prints numbers from 0 to 9
- *@a: number to be checked
  *
  *Return: number 0 to 9
  */
 void print_numbers(void)
 {
-	int a;
-
-	for (a >= 0, a <= 9, a++;)
-		_putchar(a);
-	_putchar("%d\n");
+	int a
+	for (a >= 0; a <= 9; a++)
+	_putchar("a,\n");
 }
