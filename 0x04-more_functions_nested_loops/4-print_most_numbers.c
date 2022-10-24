80770978c03ba1a4782972c7492d6e751ee9bfb1
itsjustkerry <moywaywakerry@gmail.com>
2022-10-24 07:54:35 -0700

    prints numbers without two and four

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,14 @@
 /**
  *print_most_numbers- prints zero to nine without two and four
  *without 2 and 4
- *Return: Always 0
+ *
+ *Return: no value
  */
 void print_most_numbers(void)
 {
 	int a = 0;
 
-	while (a < 10)
+	while (a <= 9)
 	{
 		if (a != '2' && a != '4')
 			_putchar(a + '0');
