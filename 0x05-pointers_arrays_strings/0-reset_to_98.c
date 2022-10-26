161bf7259f9c760d436bf6ec83d06d4161483976
itsjustkerry <moywaywakerry@gmail.com>
2022-10-26 10:10:49 -0700

    assigns value to pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - assigns value to pointer
+ *@n: pointer
+ *
+ * Return: no value
+ */
+void reset_to_98(int *n)
+{
+	int i = 0;
+
+	int *n = 98;
+
+	int *n = i;
+
+	printf("%d\n", *n);
+}
