156d8ca792d2b919511cfbe9bb89d509a9a51bc9
itsjustkerry <moywaywakerry@gmail.com>
2022-11-14 10:38:52 -0800

    links two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h> 
+
+/**
+ * _strcat- joins two strings together
+ * @src: string one
+ * @dest: string two
+ *
+ * Return: No value
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr;
+
+	printf("%s\n", src);
+	printf("%s\n", dest);
+	ptr = _strcat(src, dest);
+	printf("%s\n", src);
+	printf("%s\n", dest);
+	printf("%s\n", ptr);
+
+	return (0);
+}
