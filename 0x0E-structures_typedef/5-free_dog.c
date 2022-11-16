ea0da5d3f00c04b7f79caf077623355752ee34e2
itsjustkerry <moywaywakerry@gmail.com>
2022-11-16 10:36:41 -0800

    frees the dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dog
+ * @d: the dog
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
